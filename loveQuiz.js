42b3ca1c2c055107c001b9ade917abdb4161b446
ratul-ops <bayzedboyyah@gmail.com>
2026-07-17 23:10:48 +0600

    Update loveQuiz.js

--- loveQuiz.js
+++ loveQuiz.js
@@ -2,32 +2,31 @@ function startLoveQuiz() {
   const quizBox = document.getElementById("loveQuiz");
   const questionEl = document.getElementById("lQuestion");
   const startBtn = document.getElementById("startLoveBtn");
+  const optionsEl = document.getElementById("lOptions");
 
   // Love Quiz দেখাও
   quizBox.classList.remove("hidden");
-  questionEl.innerText = " 😊 নিচে স্ক্রল করো... অনেক দূর যাও!";
+  questionEl.innerText = "😊 ৫ সেকেন্ড অপেক্ষা করো...";
 
-  // স্ক্রল ইভেন্ট চেক করো
-  window.addEventListener("scroll", () => {
-    if (window.scrollY + window.innerHeight >= document.body.scrollHeight - 50) {
-      startBtn.classList.remove("hidden");
-    }
-  });
+  // ৫ সেকেন্ড পর বাটন অটো শো হবে
+  setTimeout(() => {
+    startBtn.classList.remove("hidden");
+  }, 5000);
 
   // বাটনে ক্লিক করলে ফানি মেসেজ + ছবি + মিউজিক
   startBtn.onclick = () => {
     questionEl.innerText = "😂 চরিত্র ভালো কর আর সোজা উপরে যা অন্যটা খেল!";
-    const optionsEl = document.getElementById("lOptions");
     optionsEl.innerHTML = "";
 
     // Funny Meme ছবি
-    let img = document.createElement("img");
+    const img = document.createElement("img");
     img.src = "funny-meme.jpg";   // ফাইলের নাম ঠিক রাখো
     img.style.maxWidth = "300px";
+    img.style.width = "100%";     // মোবাইলে ফিট হবে
     optionsEl.appendChild(img);
 
     // Background Music
-    let audio = document.createElement("audio");
+    const audio = document.createElement("audio");
     audio.src = "funny-bgm.mp3";  // ফাইলের নাম ঠিক রাখো
     audio.autoplay = true;
     audio.controls = true;
